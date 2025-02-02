833f058a87c590a7a4b72a93163f19fa7f784c88
Nuo Wen Lei <46566976+NuoWenLei@users.noreply.github.com>
2025-02-02 07:41:06 -0500

    Update page.tsx

--- src/app/[owner]/page.tsx
+++ src/app/[owner]/page.tsx
@@ -1,8 +1,8 @@
+ /* eslint-disable */
 import RepositoriesView from "@/components/RepositoriesView";
 
 export default async function RepoListPage({ params }: { params: { owner: string } }) {
     const { owner } = await params;
-    const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN!; // Store token in env variable
 
-    return <RepositoriesView owner={owner} token={token}/>;
+    return <RepositoriesView owner={owner} token={""}/>;
 }
