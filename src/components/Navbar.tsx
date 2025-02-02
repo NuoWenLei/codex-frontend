3a57b434673f000c81b7738d3614be12677f15ca
Michael Tu <michaeltu705@gmail.com>
2025-02-02 11:44:33 -0500

    changed repositories button text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar() {
           onClick={handleRedirectToRepositories}
           className="px-4 py-2 bg-gray-800 hover:bg-gray-700 rounded-lg"
         >
-          Repositories
+          Your Repositories
         </button>
       )}
 
