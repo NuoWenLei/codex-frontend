7c2bcde7b9753d709d74e3f271b4f9b47b4f7d1f
Nuo Wen Lei <46566976+NuoWenLei@users.noreply.github.com>
2025-02-02 07:45:09 -0500

    Update page.tsx

--- src/app/[owner]/page.tsx
+++ src/app/[owner]/page.tsx
@@ -2,7 +2,7 @@
 "use client"
 import RepositoriesView from "@/components/RepositoriesView";
 
-export default async function RepoListPage({ params }: { params: { owner: string } }) {
+export default async function RepoListPage({ params }: { params: Promise<{ owner: string }> }) {
     const { owner } = await params;
 
     return <RepositoriesView owner={owner} token={""}/>;
